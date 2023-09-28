ea4417ed442ab061ea1a3dcb1d512e631da94364
erik-ferreira <erikdesouzagf@gmail.com>
2023-09-28 16:14:04 -0300

    create navigation with two screens

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -1,9 +1,33 @@
+import { Feather } from "@expo/vector-icons"
 import { Drawer } from "expo-router/drawer"
 
 export default function DrawerLayout() {
   return (
-    <Drawer>
-      <Drawer.Screen name="index" />
+    <Drawer
+      screenOptions={{
+        headerShown: false,
+        drawerActiveTintColor: "#FFF",
+        drawerActiveBackgroundColor: "#2b2b2b",
+      }}
+    >
+      <Drawer.Screen
+        name="index"
+        options={{
+          drawerLabel: "Home",
+          drawerIcon: ({ size, color }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="schedules"
+        options={{
+          drawerLabel: "Agendamentos",
+          drawerIcon: ({ size, color }) => (
+            <Feather name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
     </Drawer>
   )
 }
--- src/app/(drawer)/schedules.tsx
+++ src/app/(drawer)/schedules.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View } from "react-native"
 
-export default function Home() {
+export default function Schedules() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Schedules</Text>
     </View>
   )
 }
